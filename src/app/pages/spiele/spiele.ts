d09c1028c20450393c4b6aca7034d72bea43eb34
Andrin N. Affolter <Andrin@manet.intern>
2026-09-04 00:11:07 +0200

    initial commit with base index page

---
+++ src/app/pages/spiele/spiele.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-spiele',
+  styleUrl: './spiele.css',
+  templateUrl: './spiele.html',
+})
+export class Spiele {}
---
+++ src/app/pages/team/team.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-team',
+  styleUrl: './team.css',
+  templateUrl: './team.html',
+})
+export class Team {}
